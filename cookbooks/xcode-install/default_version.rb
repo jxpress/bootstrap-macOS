e074445a129164eda566ef69c2495e4255e3c310
h.motodera <h.motodera@jxpress.net>
2019-07-05 16:36:10 +0900

    Comment out to xcversion update command

--- cookbooks/xcode-install/default_version.rb
+++ cookbooks/xcode-install/default_version.rb
@@ -1,6 +1,7 @@
-execute "update xcode-install" do
-    command "xcversion update"
-end
+# TODO: ENV check
+# execute "update xcode-install" do
+#     command "xcversion update"
+# end
 
 execute "install default xcode" do
     command "xcversion install 10.2"
