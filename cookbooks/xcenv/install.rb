7c8b1c255b4a6c0e145b9c41b53ab8529a2060e3
h.motodera <h.motodera@jxpress.net>
2019-07-05 15:55:38 +0900

    Add setting xcenv lane

--- cookbooks/xcenv/install.rb
+++ cookbooks/xcenv/install.rb
@@ -4,3 +4,13 @@ git ENV["HOME"] + "/.xcenv" do
     repository "https://github.com/xcenv/xcenv"
     not_if "test $(which xcenv)"
 end
+
+execute "set PATH xcenv" do
+    command %Q(echo 'export PATH="$HOME/.xcenv/bin:$PATH"' >> $HOME/.bash_profile)
+    not_if "cat $HOME/.bash_profile | grep '$HOME/.xcenv/bin:$PATH'"
+end
+
+execute "xcenv init" do
+    command %q(echo 'eval "$(xcenv init -)"' >> $HOME/.bash_profile)
+    not_if "cat $HOME/.bash_profile | grep 'xcenv init'"
+end
