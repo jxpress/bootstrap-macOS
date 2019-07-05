cb755a731bf4d382194a3df131d0404ae4d3e7cd
h.motodera <h.motodera@jxpress.net>
2019-07-05 15:23:17 +0900

    Add setup rbenv from rbenv install cookbook

--- cookbooks/rbenv/install.rb
+++ cookbooks/rbenv/install.rb
@@ -4,3 +4,12 @@ git ENV["HOME"] + "/.rbenv" do
     repository "https://github.com/rbenv/rbenv"
     not_if "test $(which rbenv)"
 end
+
+execute "setup rbenv" do
+    command "cd ~/.rbenv && src/configure && make -C src"
+    not_if "test $(rbenv version)"
+end
+
+execute "update rbenv" do
+    command "cd ~/.rbenv ; git checkout master ; git pull"
+end
