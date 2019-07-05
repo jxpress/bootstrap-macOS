b894f703f24de7f1992c6b31bb50e4fc991c0db0
h.motodera <h.motodera@jxpress.net>
2019-07-05 15:44:14 +0900

    Add setting rbenv lane

--- cookbooks/rbenv/install.rb
+++ cookbooks/rbenv/install.rb
@@ -5,6 +5,16 @@ git ENV["HOME"] + "/.rbenv" do
     not_if "test $(which rbenv)"
 end
 
+execute "set PATH rbenv" do
+  command %Q(echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> $HOME/.bash_profile)
+  not_if "cat $HOME/.bash_profile | grep '$HOME/.rbenv/bin:$PATH'"
+end
+
+execute "rbenv init" do
+  command %q(echo 'eval "$(rbenv init -)"' >> $HOME/.bash_profile)
+  not_if "cat $HOME/.bash_profile | grep 'rbenv init'"
+end
+
 execute "setup rbenv" do
     command "cd ~/.rbenv && src/configure && make -C src"
     not_if "test $(rbenv version)"
